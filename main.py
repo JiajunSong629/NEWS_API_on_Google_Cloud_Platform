9bbd6df0dcfa2e67926a1cdf58e4ac007c01bd5e
Jiajun629 <jiajun.song629@gmail.com>
2020-02-01 18:32:08 -0500

    test cloudbuild

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps' + ' Test!'
+    return 'Hello I like to make AI Apps' + ' Test CloudBuild!'
 
 @app.route('/name/<value>')
 def name(value):
